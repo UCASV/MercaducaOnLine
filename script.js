11b7334035ba5de89427fcadacf12f795cef0064
Valercat <00002024@uca.edu.sv>
2025-09-30 12:20:43 -0600

    add: funciones declaradas en JS y id de elementos de navbar definidos

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
+
+    // Funciones de barra de navegacion
+    inicio(
+    );
+    emprendimientos(
+    );
+    masVendidos();
+    proximos();
+    contacto();
+
+    window.inicio = inicio
+    window.emprendimientos = emprendimientos
+    window.masVendidos = masVendidos
+    window.proximos = proximos
+    window.contacto = contacto
+
+    /////////////////////////////////////////////////////
 });
