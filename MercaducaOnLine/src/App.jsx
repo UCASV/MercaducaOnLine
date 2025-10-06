bba7f33d5ecb8d21d655cc054040b9b01256ed9f
Valercat <00002024@uca.edu.sv>
2025-10-06 15:02:19 -0600

    add: html y css añadidos a app.jsx y app.css

--- MercaducaOnLine/src/App.jsx
+++ MercaducaOnLine/src/App.jsx
@@ -1,35 +1,259 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import mercadoucapneg from "./img/mercaducaopng.png";
+import mercaducachiquitob from "./img/mercaducachiquitob.png";
+import instagram from "./img/instagram.png";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <html lang="es">
+        <head>
+          <meta charset="UTF-8" />
+          <link href="style.css" rel="stylesheet" />
+          <title> Mercaduca On Line</title>
+        </head>
+        <body id="inicio">
+          <nav>
+            <ul class="navbar">
+              <img
+                class="mercaducaLogonav"
+                src={mercaducachiquitob}
+                alt="Logo de Mercaduca On Line"
+              />
+              {/* <!-- llevarlo a una zona de la pagina --> */}
+              <a class="elemento" href="#inicio">
+                <li>Inicio</li>
+              </a>
+              {/* <!-- otra pagina para eventos --> */}
+              <a class="elemento" href="#">
+                <li id="eventos">Eventos</li>
+              </a>
+              {/* <!-- otra pagina para productos --> */}
+              <a class="elemento" href="#">
+                <li id="productos" onclick="productos()">
+                  Productos
+                </li>
+              </a>
+              {/* <!-- llevarlo a una zona de la pagina --> */}
+              <a class="elemento" href="#contenedorEmprendimientos">
+                <li>Emprendimientos</li>
+              </a>
+              {/* <!-- llevarlo a una zona de la pagina --> */}
+              <a class="elemento" href="#contenedorMasVendidos">
+                <li>Productos más vendidos</li>
+              </a>
+              {/* <!-- llevarlo a una zona de la pagina --> */}
+              <a class="elemento" href="#contenedorProximos">
+                <li>Emprendimientos próximos</li>
+              </a>
+              {/* <contacto footer */}
+              <a class="elemento" href="#footer">
+                <li>Contacto</li>
+              </a>
+            </ul>
+          </nav>
+          <section>
+            <div class="contenedorLogo">
+              <img
+                class="mercaducaLogoPrincipal"
+                src={mercadoucapneg}
+                alt="Logo de Mercaduca On Line"
+              />
+            </div>
+
+            <div class="contenedorPrincipal">
+              <h1>Bienvenidos a Mercaduca On Line</h1>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+                non reprehenderit harum dicta quod odio officiis facere et natus
+                earum. Aliquam quia, aliquid illo amet a quo exercitationem
+                deserunt explicabo. Lorem ipsum dolor sit amet consectetur
+                adipisicing elit. Suscipit fugiat id optio, maiores magnam
+                asperiores distinctio labore. Assumenda modi, rerum ut,
+                similique vero non officia, obcaecati facilis quo provident
+                ipsum? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Nostrum non reprehenderit harum dicta quod odio officiis facere
+                et natus earum. Aliquam quia, aliquid illo amet a quo
+                exercitationem deserunt explicabo. Lorem ipsum dolor sit amet
+                consectetur adipisicing elit. Eligendi minus deserunt sed ex
+                aliquid debitis unde necessitatibus repellat porro veritatis
+                facilis exercitationem suscipit, quam, fugiat assumenda
+                sapiente, temporibus officia quaerat. Lorem ipsum dolor sit amet
+                consectetur adipisicing elit. Nostrum non reprehenderit harum
+                dicta quod odio officiis facere et natus earum. Aliquam quia,
+                aliquid illo amet a quo exercitationem deserunt explicabo. Lorem
+                ipsum dolor sit amet consectetur adipisicing elit. Eligendi
+                minus deserunt sed ex aliquid debitis unde necessitatibus
+                repellat porro veritatis facilis exercitationem suscipit, quam,
+                fugiat assumenda sapiente, temporibus officia quaerat. Lorem
+                ipsum dolor sit amet consectetur adipisicing elit. Nostrum non
+                reprehenderit harum dicta quod odio officiis facere et natus
+                earum. Aliquam quia, aliquid illo amet a quo exercitationem
+                deserunt explicabo. Lorem ipsum dolor sit amet consectetur
+                adipisicing elit. Eligendi minus deserunt sed ex aliquid debitis
+                unde necessitatibus repellat porro veritatis facilis
+                exercitationem suscipit, quam, fugiat assumenda sapiente,
+                temporibus officia quaerat. Lorem ipsum dolor sit amet
+                consectetur adipisicing elit. Nostrum non reprehenderit harum
+                dicta quod odio officiis facere et natus earum. Aliquam quia,
+                aliquid illo amet a quo exercitationem deserunt explicabo. Lorem
+                ipsum dolor sit amet consectetur adipisicing elit. Eligendi
+                minus deserunt sed ex aliquid debitis unde necessitatibus
+                repellat porro veritatis facilis exercitationem suscipit, quam,
+                fugiat assumenda sapiente, temporibus officia quaerat.
+              </p>
+              <section id="contenedorEmprendimientos">
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Nostrum non reprehenderit harum dicta quod odio officiis
+                  facere et natus earum. Aliquam quia, aliquid illo amet a quo
+                  exercitationem deserunt explicabo. Lorem ipsum dolor sit amet
+                  consectetur adipisicing elit. Suscipit fugiat id optio,
+                  maiores magnam asperiores distinctio labore. Assumenda modi,
+                  rerum ut, similique vero non officia, obcaecati facilis quo
+                  provident ipsum? Lorem ipsum dolor sit amet consectetur
+                  adipisicing elit. Nostrum non reprehenderit harum dicta quod
+                  odio officiis facere et natus earum. Aliquam quia, aliquid
+                  illo amet a quo exercitationem deserunt explicabo. Lorem ipsum
+                  dolor sit amet consectetur adipisicing elit. Eligendi minus
+                  deserunt sed ex aliquid debitis unde necessitatibus repellat
+                  porro veritatis facilis exercitationem suscipit, quam, fugiat
+                  assumenda sapiente, temporibus officia quaerat. Lorem ipsum
+                  dolor sit amet consectetur adipisicing elit. Nostrum non
+                  reprehenderit harum dicta quod odio officiis facere et natus
+                  earum. Aliquam quia, aliquid illo amet a quo exercitationem
+                  deserunt explicabo. Lorem ipsum dolor sit amet consectetur
+                  adipisicing elit. Eligendi minus deserunt sed ex aliquid
+                  debitis unde necessitatibus repellat porro veritatis facilis
+                  exercitationem suscipit, quam, fugiat assumenda sapiente,
+                  temporibus officia quaerat. Lorem ipsum dolor sit amet
+                  consectetur adipisicing elit. Nostrum non reprehenderit harum
+                  dicta quod odio officiis facere et natus earum. Aliquam quia,
+                  aliquid illo amet a quo exercitationem deserunt explicabo.
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Eligendi minus deserunt sed ex aliquid debitis unde
+                  necessitatibus repellat porro veritatis facilis exercitationem
+                  suscipit, quam, fugiat assumenda sapiente, temporibus officia
+                  quaerat. Lorem ipsum dolor sit amet consectetur adipisicing
+                  elit. Nostrum non reprehenderit harum dicta quod odio officiis
+                  facere et natus earum. Aliquam quia, aliquid illo amet a quo
+                  exercitationem deserunt explicabo. Lorem ipsum dolor sit amet
+                  consectetur adipisicing elit. Eligendi minus deserunt sed ex
+                  aliquid debitis unde necessitatibus repellat porro veritatis
+                  facilis exercitationem suscipit, quam, fugiat assumenda
+                  sapiente, temporibus officia quaerat.
+                </p>
+              </section>
+              <div id="contenedorMasVendidos">
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Nostrum non reprehenderit harum dicta quod odio officiis
+                  facere et natus earum. Aliquam quia, aliquid illo amet a quo
+                  exercitationem deserunt explicabo. Lorem ipsum dolor sit amet
+                  consectetur adipisicing elit. Suscipit fugiat id optio,
+                  maiores magnam asperiores distinctio labore. Assumenda modi,
+                  rerum ut, similique vero non officia, obcaecati facilis quo
+                  provident ipsum? Lorem ipsum dolor sit amet consectetur
+                  adipisicing elit. Nostrum non reprehenderit harum dicta quod
+                  odio officiis facere et natus earum. Aliquam quia, aliquid
+                  illo amet a quo exercitationem deserunt explicabo. Lorem ipsum
+                  dolor sit amet consectetur adipisicing elit. Eligendi minus
+                  deserunt sed ex aliquid debitis unde necessitatibus repellat
+                  porro veritatis facilis exercitationem suscipit, quam, fugiat
+                  assumenda sapiente, temporibus officia quaerat. Lorem ipsum
+                  dolor sit amet consectetur adipisicing elit. Nostrum non
+                  reprehenderit harum dicta quod odio officiis facere et natus
+                  earum. Aliquam quia, aliquid illo amet a quo exercitationem
+                  deserunt explicabo. Lorem ipsum dolor sit amet consectetur
+                  adipisicing elit. Eligendi minus deserunt sed ex aliquid
+                  debitis unde necessitatibus repellat porro veritatis facilis
+                  exercitationem suscipit, quam, fugiat assumenda sapiente,
+                  temporibus officia quaerat. Lorem ipsum dolor sit amet
+                  consectetur adipisicing elit. Nostrum non reprehenderit harum
+                  dicta quod odio officiis facere et natus earum. Aliquam quia,
+                  aliquid illo amet a quo exercitationem deserunt explicabo.
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Eligendi minus deserunt sed ex aliquid debitis unde
+                  necessitatibus repellat porro veritatis facilis exercitationem
+                  suscipit, quam, fugiat assumenda sapiente, temporibus officia
+                  quaerat. Lorem ipsum dolor sit amet consectetur adipisicing
+                  elit. Nostrum non reprehenderit harum dicta quod odio officiis
+                  facere et natus earum. Aliquam quia, aliquid illo amet a quo
+                  exercitationem deserunt explicabo. Lorem ipsum dolor sit amet
+                  consectetur adipisicing elit. Eligendi minus deserunt sed ex
+                  aliquid debitis unde necessitatibus repellat porro veritatis
+                  facilis exercitationem suscipit, quam, fugiat assumenda
+                  sapiente, temporibus officia quaerat.
+                </p>
+              </div>
+              <div id="contenedorProximos">
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Nostrum non reprehenderit harum dicta quod odio officiis
+                  facere et natus earum. Aliquam quia, aliquid illo amet a quo
+                  exercitationem deserunt explicabo. Lorem ipsum dolor sit amet
+                  consectetur adipisicing elit. Suscipit fugiat id optio,
+                  maiores magnam asperiores distinctio labore. Assumenda modi,
+                  rerum ut, similique vero non officia, obcaecati facilis quo
+                  provident ipsum? Lorem ipsum dolor sit amet consectetur
+                  adipisicing elit. Nostrum non reprehenderit harum dicta quod
+                  odio officiis facere et natus earum. Aliquam quia, aliquid
+                  illo amet a quo exercitationem deserunt explicabo. Lorem ipsum
+                  dolor sit amet consectetur adipisicing elit. Eligendi minus
+                  deserunt sed ex aliquid debitis unde necessitatibus repellat
+                  porro veritatis facilis exercitationem suscipit, quam, fugiat
+                  assumenda sapiente, temporibus officia quaerat. Lorem ipsum
+                  dolor sit amet consectetur adipisicing elit. Nostrum non
+                  reprehenderit harum dicta quod odio officiis facere et natus
+                  earum. Aliquam quia, aliquid illo amet a quo exercitationem
+                  deserunt explicabo. Lorem ipsum dolor sit amet consectetur
+                  adipisicing elit. Eligendi minus deserunt sed ex aliquid
+                  debitis unde necessitatibus repellat porro veritatis facilis
+                  exercitationem suscipit, quam, fugiat assumenda sapiente,
+                  temporibus officia quaerat. Lorem ipsum dolor sit amet
+                  consectetur adipisicing elit. Nostrum non reprehenderit harum
+                  dicta quod odio officiis facere et natus earum. Aliquam quia,
+                  aliquid illo amet a quo exercitationem deserunt explicabo.
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Eligendi minus deserunt sed ex aliquid debitis unde
+                  necessitatibus repellat porro veritatis facilis exercitationem
+                  suscipit, quam, fugiat assumenda sapiente, temporibus officia
+                  quaerat. Lorem ipsum dolor sit amet consectetur adipisicing
+                  elit. Nostrum non reprehenderit harum dicta quod odio officiis
+                  facere et natus earum. Aliquam quia, aliquid illo amet a quo
+                  exercitationem deserunt explicabo. Lorem ipsum dolor sit amet
+                  consectetur adipisicing elit. Eligendi minus deserunt sed ex
+                  aliquid debitis unde necessitatibus repellat porro veritatis
+                  facilis exercitationem suscipit, quam, fugiat assumenda
+                  sapiente, temporibus officia quaerat.
+                </p>
+              </div>
+            </div>
+          </section>
+          <footer id="footer">
+            <h3>Contactanos</h3>
+            <p>
+              Telefono: 123-456-7890
+              <br />
+              Email: mercaduca@gmail.com
+              <br />
+              Direccion: UCA, El Salvador
+              <br />
+              Encuentranos en
+              <img class="icono" src={instagram} alt="Instagram" />
+              <a class="link" href="#">
+                Instagram!
+              </a>
+            </p>
+          </footer>
+          <script src="script.js"></script>
+        </body>
+      </html>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
