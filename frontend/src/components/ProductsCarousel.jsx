dc72aeece3bef7eeb2c56089d9cbf5eda114aa7e
FiorellaMaria <00149524@uca.edu.sv>
2025-11-27 03:48:59 -0600

    feat: terminado home

--- frontend/src/components/ProductsCarousel.jsx
+++ frontend/src/components/ProductsCarousel.jsx
@@ -152,7 +152,7 @@ useEffect(() => {
   return (
     <>
       <div className={styles["products-carousel"]}>
-        <h2>Top 1 de cada categoría</h2>
+        <h2 className={styles.title}>Top 1 de cada categoría</h2>
 
         <div className={styles["carousel-wrapper"]}>
           <button className={styles.arrow} onClick={() => scrollByWidth(-1)}>
